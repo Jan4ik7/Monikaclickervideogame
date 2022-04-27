fd6eb829c77a03f5f66b0630f5df69cb84fb5eae
Jan4ik7 <59110966+Jan4ik7@users.noreply.github.com>
2022-04-27 17:58:49 +0300

    Add files via upload

---
+++ Dialog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    public Text textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+
+
+    void Start()
+    {
+        index = PlayerPrefs.GetInt("index", index);
+        StartCoroutine(Type());
+    }
+    private void Update()
+    {
+        PlayerPrefs.SetInt("index", index);
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+    public void NextSentence() 
+    {
+
+        continueButton.SetActive(false);
+    
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+
+        }
+
+    }
+}
---
+++ game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class game : MonoBehaviour
+{
+    public Text scoreText;
+    public Text moneyText;
+    public GameObject delete;
+    private int money;
+    private int score;
+    public GameObject ShopPan;
+    public Text gameoverText;
+    
+    
+    public void Update()
+    {
+        scoreText.text = score + "";
+        moneyText.text = money + "$";
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("score", score);
+    }
+    public void ShopPan_ShowAndHIde()
+    {
+        ShopPan.SetActive(!ShopPan.activeSelf);
+    }
+
+    public void OnClick1()
+    {
+        if (money >= 20)
+        {
+            Destroy(delete);
+            gameoverText.text = "Game over";
+            money -= 20;
+        }
+
+    }
+    
+    public void OnClcick()
+    {
+        score++;
+        scoreText.text = score + "";
+        money++;
+        moneyText.text = money + "$";
+
+    }
+     void Start()
+    {
+        money = PlayerPrefs.GetInt("money", money);
+        score = PlayerPrefs.GetInt("score", score);
+    }
+}
